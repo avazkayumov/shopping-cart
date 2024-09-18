13161c18f25b576ebabc01b4ed6cc6694dd0233e
Avaz <avaz@avazs-mbp.localdomain>
2024-09-18 11:55:19 +0200

    localstorage

--- script.js
+++ script.js
@@ -31,11 +31,12 @@ let shopItemsData = [
     }
 ]
 
-let basket = []
+let basket = JSON.parse(localStorage.getItem("data")) || []
 
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((item) => {
         let {img, name, price, desc, id} = item
+        let search = basket.find((x) => x.id === id ) || [];
     return `
         <div class="item" id="product-id-${id}">
             <img width="200" src="./${img}" alt="">
@@ -47,7 +48,7 @@ let generateShop = () => {
                 <h2 class="price">$ ${price}</h2>
                 <div class="buttons">
                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id="${id}" class="quantity">0</div>
+                    <div id="${id}" class="quantity">${search.item === undefined ? 0 : search.item}</div>
                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                 </div>
             </div>
@@ -70,7 +71,7 @@ const increment = (id) => {
     }else {
         search.item += 1
     }
-    // console.log(basket)
+    localStorage.setItem('data', JSON.stringify(basket))
 
     update(selectedItem.id)
 }
@@ -85,10 +86,22 @@ const decrement = (id) => {
     }
     // console.log(basket)
 
+    localStorage.setItem('data', JSON.stringify(basket))
+
     update(selectedItem.id)
 } 
 
 const update = (id) => {
     let search = basket.find((x) => x.id === id)
     document.getElementById(id).innerHTML = search.item
+
+    calculation()
 }
+
+let calculation  = () => {
+    let cartIcon = document.getElementById('cartAmount')
+
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((acc, x) => acc + x, 0)
+}
+
+calculation()
